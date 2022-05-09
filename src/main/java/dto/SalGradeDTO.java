a90907c2c768ca2695da670054f55695a2de463a
David Oh <davidoh72@gmail.com>
2022-05-09 11:59:47 +0900

    22.05.09

--- src/main/java/dto/SalGradeDTO.java
+++ src/main/java/dto/SalGradeDTO.java
@@ -2,12 +2,14 @@ package dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 import java.util.List;
 
 @Setter
 @Getter
 //@AllArgsConstructor << don't add this.. won't work with 1:1 or 1:M injections (association or collection)
+@ToString(exclude={"empDTOList"})
 public class SalGradeDTO {
     private int grade;
     private int loSal;
